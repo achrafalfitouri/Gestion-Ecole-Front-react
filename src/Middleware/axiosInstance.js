23f09ccfe34edde1d529d154685f1d75ca9c320d
achrafalfitouri <84252969+achrafalfitouri@users.noreply.github.com>
2024-07-03 16:17:18 +0100

    login et middlware refresh

--- src/Middleware/axiosInstance.js
+++ src/Middleware/axiosInstance.js
@@ -22,12 +22,43 @@ axiosInstance.interceptors.response.use(
     return response;
   },
   (error) => {
-    if (error.response.status === 401) {
+    if (error.response && error.response.status === 401) {
       localStorage.removeItem('token');
       window.location.href = '/login';
     }
     return Promise.reject(error);
   }
 );
+
+const refreshToken = async () => {
+  try {
+    const oldToken = localStorage.getItem('token');
+    if (!oldToken) {
+      throw new Error('No token found');
+    }
+
+    console.log('Refreshing token...');
+    const response = await axios.post('http://127.0.0.1:3000/api/auth/refresh-token', null, {
+      headers: {
+        'Authorization': `${oldToken}` // Ensure 'Bearer ' prefix if required by your server
+      }
+    });
+
+    const { token: newToken } = response.data;
+
+    // Update token in localStorage immediately
+    localStorage.setItem('token', newToken);
+
+    // Optional: Trigger a state update if using React or similar framework
+    // dispatch({ type: 'SET_TOKEN', payload: newToken });
+  } catch (error) {
+    localStorage.removeItem('token');
+  }
+};
+
+// Set interval to refresh token every 55 minutes
+setInterval(() => {
+  refreshToken();
+}, 55 * 60 * 1000);
 
 export default axiosInstance;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,12 @@
-import React, { useContext } from 'react';
-import { Form, Input, Button, message,Row,Col,Card,Checkbox } from 'antd';
+import React, { useContext, useState } from 'react';
+import { Form, Input, Button, message, Row, Col, Card, Checkbox, Typography } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import axiosInstance from '../../Middleware/axiosInstance';
 import { UserContext } from '../../UserContextData/UserContext';
 import { Navigate } from "react-router-dom";
-import { useState } from "react";
-
-
 
 const { Item } = Form;
+const { Title, Text } = Typography;
 
 const Login = () => {
   const { setUser } = useContext(UserContext);
@@ -19,70 +17,91 @@ const Login = () => {
     try {
       const response = await axiosInstance.post('/api/auth/login', values);
       localStorage.setItem('token', response.data.token);
+      localStorage.setItem('expireIn', response.data.expiresIn);
       message.success('Login successful!');
-  
+
       const userResponse = await axiosInstance.get('/api/auth/me');
       localStorage.setItem('user', JSON.stringify(userResponse.data)); // Store as JSON string
       setUser(userResponse.data);
-      
+
       setNavigate(true); // Redirect to home or dashboard
     } catch (error) {
       message.error('Login failed. Please check your credentials.');
     }
   };
-  
+
   if (navigate) {
     return <Navigate to="/" />;
   }
+
   return (
-    <Row justify="center" align="middle" style={{ height: '100vh' }}>
-      <Col xs={{ span: 6, offset: 0 }}>
-        <div className="dark-theme-container" style={{ marginTop: '-100px' }}>
-          <Card title="Bonjour , Veuillez faire la conexion" className="form-card">
-            <Form form={form} onFinish={onFinish}>
-              <Item
-                name="Email"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Veuillez saisir votre email!',
-                  },
-                ]}
-                style={{ width: '100%' }}
-              >
-                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
-              </Item>
-
-              <Item
-                name="MotDePasse"
-                rules={[
-                  {
-                    required: true,
-                    message: 'Veuillez saisir votre mot de passe!',
-                  },
-                ]}
-                style={{ width: '100%' }}
-              >
-                <Input.Password
-                  prefix={<LockOutlined className="site-form-item-icon" />}
-                  placeholder="Mot de passe "
-                />
-              </Item>
-
-              <Item>
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                  <Checkbox>Enregistrer </Checkbox>
-                </Form.Item>
-              </Item>
-
-              <Item>
-                <Button type="primary" htmlType="submit" className="login-form-button">
-                  conexion
-                </Button>
-              </Item>
-            </Form>
-          </Card>
+    <Row justify="center" align="middle" style={{ height: '100vh', backgroundColor: '#fff' }}>
+    <Col xs={24} md={12} style={{ textAlign: 'center', padding: '50px', backgroundColor: '#ffffff' }}>
+      <div style={{ marginBottom: '30px' }}>
+        <div style={{ fontSize: '48px', fontWeight: 'bold', fontFamily: 'Helvetica, Arial, Futura' }}>
+          EHPM
         </div>
+      </div><br /><br />
+      <Title level={2} style={{ fontSize: '24px', fontWeight: 'bold',color:'#636261' }}>Gérez votre école avec :</Title><br />
+      <ul style={{ textAlign: 'left', fontSize: '18px', lineHeight: '1.5', fontWeight: 'bold', listStyle: 'none' ,color:'#636261'}}>
+        <li>
+        <Text strong style={{ display: 'inline-block', marginRight: '10px' }}>✔</Text> 
+
+          Outil tout-en-un<br />
+          Simplifiez la gestion de la scolarité et des finances de votre établissement.
+</li><br />
+        <li>
+        <Text strong style={{ display: 'inline-block', marginRight: '10px' }}>✔</Text> 
+
+        Centralisez la gestion de vos étudiants, des enseignants, des paiements, et bien plus.
+</li>
+      </ul>
+    </Col>
+      <Col xs={24} md={12} style={{ padding: '50px' }}>
+        <Card   title={<strong style={{ fontSize: '35px' }}>Se connecter</strong>} style={{ width: '70%' }}><br /><br />
+          <Form form={form} onFinish={onFinish} layout="vertical">
+            <Item
+              name="Email"
+              label={<Text strong style={{ fontSize: '16px' }}>Email</Text>}
+              rules={[
+                {
+                  required: true,
+                  message: 'Veuillez saisir votre email!',
+                },
+              ]}
+              
+            >
+              <Input strong               style={{ fontSize: '16px', width: '100%', minHeight: '50px' ,borderRadius:"20px"}}   prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+            </Item>
+            <br />
+            <Item
+              name="MotDePasse"
+              label={<Text strong style={{ fontSize: '16px' }}>Mot de passe</Text>}
+              rules={[
+                {
+                  required: true,
+                  message: 'Veuillez saisir votre mot de passe!',
+                },
+              ]}
+            >
+              <Input.Password
+              strong
+              style={{ fontSize: '16px', width: '100%', minHeight: '50px' ,borderRadius:"20px"}} 
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                placeholder="Mot de passe"
+              />
+            </Item>
+
+
+            <br />
+            <Item>
+              <Button type="primary" htmlType="submit" style={{ fontSize: '20px', width: '100%', minHeight: '45px',fontWeight:"bold",borderRadius:"15px" }}               >
+                Se connecter
+              </Button>
+             
+            </Item>
+          </Form>
+        </Card>
       </Col>
     </Row>
   );
